4af4aefa61f9eada97f098f127de7847a96d4918
Gaana-3099 <151710064+Gaana-3099@users.noreply.github.com>
2023-11-23 09:58:35 +0530

    Add files via upload

---
+++ contact us.js
@@ -0,0 +1,20 @@
+﻿﻿function handleSubmit () {
+    const name = document.getElementById('name').value;
+    const surname = document.getElementById('surname').value;
+    const email = document.getElementById('email').value;
+    const query = document.getElementById('query').value;
+
+
+    // to set into local storage
+    /* localStorage.setItem("NAME", name);
+    localStorage.setItem("SURNAME", surname);
+    localStorage.setItem("EMAIL", email);
+    localStorage.setItem("QUERY", query); */
+    
+    sessionStorage.setItem("NAME", name);
+    sessionStorage.setItem("SURNAME", surname);
+    sessionStorage.setItem("EMAIL", email);
+    sessionStorage.setItem("QUERY", query);
+
+    return;
+}
---
+++ contactus.js
@@ -0,0 +1,28 @@
+﻿
+function validateForm(){
+var name = document.contactus.Name;
+var email = document.contactus.Email;
+var query = document.contactus.Query;
+
+
+var count = 0;
+if (name.value === "") {
+name.placeholder="Please fill this field";
+name.style.border = "2px solid red";
+count++;
+}
+   if (email.value === "") {
+email.placeholder="Please fill this field";
+email.style.border = "2px solid red";
+count++;
+}
+if (query.value === "") {
+query.placeholder="Please fill this field";
+query.style.border = "2px solid red";
+count++;
+}
+
+
+if(count>0)
+ return false;
+}
